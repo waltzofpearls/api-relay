0c4797c1520bd3d3da36368749b1847b6dac33df
Rollie Ma <rollie.ma@gmail.com>
2015-08-14 23:59:43 -0700

    Simplify listening address and downstream setters

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import "github.com/waltzofpearls/relay-api/rapi"
 
 func main() {
 	api := rapi.New("/v1").
-		setListenProtocol("http").
-		setListenAddr("localhost:8080").
-		setDownstreamProtocol("http").
-		setDownstreamAddr("localhost:8094")
+		setListenAddr("http://localhost:8080").
+		setDownstreamAddr("http://localhost:8094")
 	api.NewEndpoint("GET", "/users")
 	api.Run()
 }
